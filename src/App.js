2f8e04bcf139f5545fc15d38a067745d001ad2ea
yarynakorduba <korduba_y@ucu.edu.ua>
2018-12-22 12:42:44 +0200

    initial commit, making beeswarm chart

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+import Layout from "./Components/Layout"
+
+import "./App.scss"
+
+class App extends Component {
+  render() {
+      return <div className="App"><Layout className={"App"}/></div>
+  }
+}
+
+export default App
