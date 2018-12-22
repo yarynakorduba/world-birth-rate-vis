e4e5e9b99c4ac5f794811ac3fb6a029c1802d93e
yarynakorduba <korduba_y@ucu.edu.ua>
2018-12-22 13:58:35 +0200

    Updates

---
+++ precache-manifest.786f884d6a33a091c43174400a01a163.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "491666cec293cf226912",
+    "url": "/world-birth-rate-vis/static/js/runtime~main.491666ce.js"
+  },
+  {
+    "revision": "c62134104e897a3a8a04",
+    "url": "/world-birth-rate-vis/static/js/main.c6213410.chunk.js"
+  },
+  {
+    "revision": "6d107bc6cb73026b52dd",
+    "url": "/world-birth-rate-vis/static/js/1.6d107bc6.chunk.js"
+  },
+  {
+    "revision": "c62134104e897a3a8a04",
+    "url": "/world-birth-rate-vis/static/css/main.dd723c7f.chunk.css"
+  },
+  {
+    "revision": "7efbec91eb91a16c6af9e0d2a09c9fb8",
+    "url": "/world-birth-rate-vis/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/world-birth-rate-vis/precache-manifest.786f884d6a33a091c43174400a01a163.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/world-birth-rate-vis/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
